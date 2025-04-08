fae2c1b33fd34215e3c2755eae2c73a7c87a98c8
shivani <shivaniw69@gmail.com>
2025-04-08 18:33:54 +0530

    form

--- hookapp/src/App.jsx
+++ hookapp/src/App.jsx
@@ -1,6 +1,5 @@
 
-//=============================================USESTATE-SETNAME=========================================================================
-
+//=============================================USESTATE-SETNAME(07-04-2025)=========================================================================
 // import { useState } from "react";
 
 // const App=()=>{
@@ -162,19 +161,193 @@
 
 //==============================================USEEFFECT+SETTIMEOUT=============================================================================
 
-import {useState,useEffect} from "react";
-const App=()=>{
-  const [count,setcount]=useState(0);
-  useEffect(()=>{
-    setTimeout(()=>{
-      setcount(count+1)
-    },2000)
+// import {useState,useEffect} from "react";
+// const App=()=>{
+//   const [count,setcount]=useState(0);
+//   useEffect(()=>{
+//     setTimeout(()=>{
+//       setcount(count+1)
+//     },2000)
     
-  })
+//   })
+//   return(
+//     <>
+//       <h1>Welocme to counting:{count}</h1>
+//     </>
+//   )
+// }
+// export  default App;
+
+//===========================================08-04-2025(work-in-home)==================================================================================================
+// import { useState,useEffect } from "react";
+
+// const App=()=>{
+//   const [count,setCount]=useState(0);
+//   useEffect(()=>{
+//     setTimeout(()=>{
+//       setCount(count+1);
+//     },2000)
+   
+//   })
+//   return(
+//     <>
+//      <h2>Welcome To:{count}</h2>
+//     </>
+//   )
+// }
+// export default App;
+//======================================================================================================================================
+// import { useState } from "react";
+// // import { Button } from "bootstrap";
+
+// const App=()=>{
+//   const[count,setCount]=useState(0);
+//   const Dec=()=>{
+//     if(count<=0)
+//     {
+//       alert("count less than 0.");
+//     }
+//     else{
+//       setCount(count-1);
+//     }
+//   }
+//   return(
+//     <>
+//     <h1 align="center">My Counter App</h1>
+//     <div id="box2">
+//        <h2>Count:{count}</h2>
+//        <button onClick={()=>{setCount(count+1)}}>Increment</button>
+//        <button onClick={Dec}>Decrement</button>
+//        <button onClick={()=>{setCount(0)}}>Reset</button>
+
+//     </div>
+//     </>
+//   )
+// }
+// export default App;
+//===========================================================================================================================
+// import { useState } from "react";
+// const App=()=>{
+//   const[name,setName]=useState("jiyaa");
+
+//   return(
+//     <>
+//       <h2>Welcome to our world {name}</h2>
+//       <button onClick={()=>{setName("Shivani")}}>Click me</button>
+//     </>
+//   )
+// }
+// export default App;
+
+
+//=============================================(08-04-2025=class)=====================================================================
+// import { useState,useEffect } from "react";
+// const App=()=>{
+//   const[count,setCount]=useState(0);
+//   useEffect(()=>{
+//   setTimeout(() => {
+//      setCount(count+1);
+//   }, 5000);
+//   })
+
+//   return(
+//     <>
+//      <h2>react example to:{count}</h2>
+//      
+//     </>
+//   )
+// }
+// export default App;
+//==============================================================================================================
+// import { useState,useEffect } from "react";
+// const App=()=>{
+//   const[count,setCount]=useState(0);
+//   useEffect(()=>{
+//   setTimeout(() => {
+//      setCount(count+1);
+//   }, 5000);
+//   },[])
+
+//   return(
+//     <>
+//      <h2>react example to:{count}</h2>
+    
+//     </>
+//   )
+// }
+// export default App;
+
+//==========================================================================================================================
+
+// import { useState,useEffect } from "react";
+// const App=()=>{
+//   const[count,setCount]=useState(0);
+//   const[multi,setMulti]=useState(0);
+  
+
+//   useEffect(()=>{
+//     setMulti(count*2)
+//   },[count])
+
+//   return(
+//     <>
+//      <h2>My Counter:{count}</h2>
+//      <h3>Multiplication:{multi}</h3>
+//      <button onClick={()=>{setCount(count+1)}}>Click Here</button>
+    
+//     </>
+//   )
+// }
+// export default App;
+
+//======================================================================================================================
+
+// import { useState } from "react";
+// const App=()=>{
+//   const[name,setName]=useState("");
+// const[city,setCity]=useState("");
+
+// const handleSubmit=()=>{
+//   alert(`Name: ${name} City: ${city}`)
+// }
+//   return(
+//     <>
+
+//     <h2>Application Form</h2>
+//     name: <input type="text" value={name}
+//     onChange={(e)=>{setName(e.target.value)}} />
+//     <br/><br/>
+//     city: <input type="text" value={city}
+//     onChange={(e)=>{setCity(e.target.value)}} />
+
+//     <button onClick={handleSubmit}>Submit</button>
+//     </>
+//   )
+// }
+// export default App;
+
+
+//==============================================================================================================================
+import { useState } from "react";
+const App=()=>{
+  const[input,setInput]=useState({});
+  const handleinput=(e)=>{
+    let name=e.target.name;
+    let value=e.target.value;
+
+     setInput(values=>({...values,[name]:value}))
+     console.log(input);
+}
   return(
     <>
-      <h1>Welocme to counting:{count}</h1>
+      <h2>Application form</h2>
+      Rollno:<input type="text"  name="rollno" onChange={handleinput}/><br/><br/>
+      Name:<input type="text"  name="name" onChange={handleinput}/><br/><br/>
+      City:<input type="text"  name="city" onChange={handleinput}/><br/><br/>
+      Fees:<input type="text"  name="fees" onChange={handleinput}/><br/><br/>
+      <button onClick={handleinput}>Submit</button>
+
     </>
   )
 }
-export  default App;
+export default App;
