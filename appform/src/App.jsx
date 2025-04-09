135c19c37a4384554fd6f4c1951409d27a401c39
shivani <shivaniw69@gmail.com>
2025-04-09 23:14:17 +0530

    json

--- appform/src/App.jsx
+++ appform/src/App.jsx
@@ -20,25 +20,55 @@
 
 //=======================================FORM(USE-SPREADOPRATOR)======================================================================================================
 
-import { useState } from "react";
-const App=()=>{
-  const[input,setInput]=useState({});
-  const handleInput=(e)=>{
-    let name=e.target.name;
-    let value=e.target.value;
+// import { useState } from "react";
+// const App=()=>{
+//   const[input,setInput]=useState({});
+//   const handleInput=(e)=>{
+//     let name=e.target.name;
+//     let value=e.target.value;
     
-    setInput(values=>({...values,[name]:value}))
-    console.log(input);
-  }
-  return(
-    <>
-    <h1>Application Form</h1>
-    Name: <input type="text" name="name"  onChange={handleInput}/><br/><br/>
-    Roll: <input type="text" name="rollno"  onChange={handleInput}/><br/><br/>
-    City: <input type="text" name="city"  onChange={handleInput}/><br/><br/>
-    Fees: <input type="text" name="fees"  onChange={handleInput}/><br/><br/>
-    <button onClick={handleInput}>Submit</button>
-    </>
-  )
+//     setInput(values=>({...values,[name]:value}))
+//     console.log(input);
+//   }
+//   return(
+//     <>
+//     <h1>Application Form</h1>
+//     Name: <input type="text" name="name"  onChange={handleInput}/><br/><br/>
+//     Roll: <input type="text" name="rollno"  onChange={handleInput}/><br/><br/>
+//     City: <input type="text" name="city"  onChange={handleInput}/><br/><br/>
+//     Fees: <input type="text" name="fees"  onChange={handleInput}/><br/><br/>
+//     <button onClick={handleInput}>Submit</button>
+//     </>
+//   )
+// }
+// export default App;
+
+//============================================(09-04-2025(work-in home))============================================================================================================
+import { useState } from "react";
+import axios from "axios";
+const App=()=>{
+    const[input,setInput]=useState({});
+    const handleInput=(e)=>{
+        let name=e.target.name;
+        let value=e.target.value;
+        setInput((values)=>({...values,[name]:value}))
+        console.log(input);
+    }
+    const handleSubmit=async()=>{
+       let api="http://localhost:3000/data";
+       const response=await axios.post(api,input);
+       alert("data succesfully save!!");
+    }
+
+    return(
+        <>
+        <h2>Application Form</h2>
+        Name: <input type="text"  name="name"  onChange={handleInput} /> <br/><br/>
+        Roll: <input type="text"  name="rollno"  onChange={handleInput}/> <br/><br/>
+        City: <input type="text"  name="city"  onChange={handleInput} /> <br/><br/>
+        Fees: <input type="text"  name="fees"  onChange={handleInput}/> <br/><br/>
+        <button onClick={handleSubmit}>Submit!!!</button>
+        </>
+    )
 }
 export default App;
