dbdf472387bc9209dc1a9084c7b98676730c29f6
shivani <shivaniw69@gmail.com>
2025-04-24 18:27:21 +0530

    memo

--- raphookapp/src/App.jsx
+++ raphookapp/src/App.jsx
@@ -21,6 +21,8 @@
 // }
 // export default App;
 
+
+
 //==================================================================================================================
 // import { useRef } from "react";
 // const App=()=>{
@@ -58,33 +60,135 @@
 // }
 // export default  App;
 //========================================================================================================================================
-import { useRef } from "react";
-const App=()=>{
-  const myRef=useRef();
-  const back=()=>{
+// import { useRef } from "react";
+// const App=()=>{
+//   const myRef=useRef();
+//   const back=()=>{
    
-    myRef.current.style.backgroundColor="yellow";
-  }
-  const text=()=>{
-    myRef.current.innerHTML="we are cybrom student";
-    myRef.current.style.color="red";
-    myRef.current.style.padding="50px";
-  }
-  const border=()=>{
-    myRef.current.style.border="2px solid red";
-    myRef.current.style.borderRadius="100%";
+//     myRef.current.style.backgroundColor="yellow";
+//   }
+//   const text=()=>{
+//     myRef.current.innerHTML="we are cybrom student";
+//     myRef.current.style.color="red";
+//     myRef.current.style.textAlign="center";
+//     myRef.current.style.display="flex";
+//     myRef.current.style.alignItems="center";
+//   }
+//   const border=()=>{
+//     myRef.current.style.border="2px solid red";
+//     myRef.current.style.borderRadius="100%";
    
     
-  }
+//   }
+//   return(
+//     <>
+//    <div ref={myRef} style={{width:"200px",height:"200px",border:"1px solid black"}}>
+     
+//    </div>
+//    <button onClick={back} >Color</button> 
+//    <button onClick={text}> Text</button>
+//    <button onClick={border}> border</button>
+     
+//     </>
+//   )
+// }
+// export default App;
+
+
+//============================================(24-04-2025[used demo hook])=======================================================================================================================
+// import { useState } from "react";
+// const App=()=>{
+//   const[add,setAdd]=useState(0);
+//   const[sub,setSub]=useState(100);
+//   // const myMulti=useMemo(()=>{
+//   //      console.log()
+//   // },[add])
+//   function myMulti(){
+//     console.log("*****");
+//     return add*2
+//   }
+//   return(
+//     <>
+//       <h1>Welcome </h1>
+//       <h2>Addition:{add}</h2>
+//       <button onClick={()=>{setAdd(add+1)}}>Add</button>
+//       <h2>Substraction:{sub}</h2>
+//       <button onClick={()=>{setSub(sub-1)}}>Minus</button>
+//       <hr />
+//       <h2>Multiplication:{myMulti()}</h2>
+//     </>
+//   )
+// }
+// export default App;
+
+//===================================================================================================================================================================
+// import { useMemo } from "react";
+// import { useState } from "react";
+// const App=()=>{
+//   const[add,setAdd]=useState(0);
+//   const[sub,setSub]=useState(100);
+//   const myMulti=useMemo(()=>{
+//     console.log("*****");
+//     return add*2
+//   },[add])
+  
+//   return(
+//     <>
+//       <h1>Welcome </h1>
+//       <h2>Addition:{add}</h2>
+//       <button onClick={()=>{setAdd(add+1)}}>Add</button>
+//       <h2>Substraction:{sub}</h2>
+//       <button onClick={()=>{setSub(sub-1)}}>Minus</button>
+//       <hr />
+//       <h2>Multiplication:{myMulti}</h2>
+//     </>
+//   )
+// }
+// export default App;
+
+// import { useState,useMemo } from "react";
+// const App=()=>{
+//   const[mynum,setMynum]=useState(0);
+//   const[name,setName]=useState("");
+//   function myMulti(){
+//     for(var i=0; i<10000000;i++){}
+//     return mynum*2;
+//   }
+//   return(
+//     <>
+
+//       <h1>Welcome!!!</h1>
+//       Select Number: <input type="number" value={mynum}  
+//       onChange={(e)=>{setMynum(e.target.value)}}/>
+//        Select Name: <input type="text" value={name}  
+//       onChange={(e)=>{setName(e.target.value)}}/>
+//       <hr />
+//       <h1>Multiplication:{myMulti()}</h1>
+//     </>
+//   )
+// }
+// export default App;
+
+//====================================================expencive====================================================================================================
+
+import { useState,useMemo } from "react";
+const App=()=>{
+  const[mynum,setMynum]=useState(0);
+  const[name,setName]=useState("");
+  const myMulti=useMemo(()=>{
+    for(var i=0; i<10000000;i++){}
+    return mynum*2;
+  },[mynum])
   return(
     <>
-   <div ref={myRef} style={{width:"200px",height:"200px",border:"1px solid black"}}>
-     
-   </div>
-   <button onClick={back} >Color</button> 
-   <button onClick={text}> Text</button>
-   <button onClick={border}> border</button>
-     
+
+      <h1>Welcome!!!</h1>
+      Select Number: <input type="number" value={mynum}  
+      onChange={(e)=>{setMynum(e.target.value)}}/>
+       Select Name: <input type="text" value={name}  
+      onChange={(e)=>{setName(e.target.value)}}/>
+      <hr />
+      <h1>Multiplication:{myMulti}</h1>
     </>
   )
 }
