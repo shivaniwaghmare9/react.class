8c3701631e530f004ed7817a6d24ac446d5f6828
shivani <shivaniw69@gmail.com>
2025-05-10 02:34:00 +0530

    slice

--- shop5pm/src/cartSlice.jsx
+++ shop5pm/src/cartSlice.jsx
@@ -1,4 +1,4 @@
-
+//import { ToastContainer, toast } from 'react-toastify';
 import { createSlice } from "@reduxjs/toolkit";
 const cartSlice=createSlice({
     name:"mycart",
@@ -7,11 +7,54 @@ const cartSlice=createSlice({
     },
     reducers:{
         addCart:(state,actions)=>{
-        // state.cart=state.cart.filter(key=>key.id!=actions.payload.id)
-        state.cart.push(actions.payload);
+          const mydata= state.cart.filter(key=>key.id==actions.payload.id);
+            if (mydata.length>=1)
+            {
+                alert("This Product Aleready Added!");
+            }
+            else 
+            {
+                state.cart.push(actions.payload);
+            } 
            
+        },
+    qntyIncr:(state,actions)=>{
+        for(var i=0; i<state.cart.length; i++)
+        {
+            if(state.cart[i].id==actions.payload.id)
+            {
+                state.cart[i].qnty++;
+            }
         }
+    },
+    qntyDecr:(state,actions)=>{
+        for(var i=0; i<state.cart.length; i++)
+        {
+            if(state.cart[i].id==actions.payload.id)
+            {
+                if(state.cart[i].qnty<=1)
+                {
+                    alert("Quantity not less than 1")
+                }
+                else
+                {
+                    state.cart[i].qnty--;
+                }
+            }
+            
+        }
+    },
+    remove:(state,actions)=>{
+      state.cart=state.cart.filter(key=>key.id!=actions.payload.id)
     }
+
+    
+    }
+    
+    
+
 })
-export const{addCart}=cartSlice.actions;
+
+
+export const{addCart,qntyDecr,qntyIncr,remove}=cartSlice.actions;
 export default cartSlice.reducer;
--- shop5pm/src/pages/MyCart.jsx
+++ shop5pm/src/pages/MyCart.jsx
@@ -1,8 +1,12 @@
 import { useSelector,useDispatch } from "react-redux";
 import Table from 'react-bootstrap/Table';
+import { FaPlusCircle } from "react-icons/fa";
+import { FaMinusCircle } from "react-icons/fa";
+import { qntyDecr,qntyIncr,remove } from "../cartSlice";
+
 const MyCart=()=>{
     const cartData=useSelector(state=>state.mycart.cart);
-    //const dispatch=useDispatch();
+    const dispatch=useDispatch();
     const ans=cartData.map((key)=>{
         return(
             <>
@@ -12,7 +16,18 @@ const MyCart=()=>{
                 </td>
                 <td>{key.Name}</td>
                 <td>{key.category}</td>
-                <td>{key.price}</td>
+                <td style={{fontSize:"20px"}}> 
+                <FaMinusCircle   onClick={()=>{dispatch(qntyDecr({id:key.id}))}} style={{cursor:"pointer"}} />   
+                    {key.qnty}
+                    <FaPlusCircle 
+                onClick={()=>{dispatch(qntyIncr({id:key.id}))}} style={{cursor:"pointer"}} />
+                </td>
+                <td>
+                  {key.qnty*key.price}
+                  </td>
+                  <td>
+                     <button onClick={()=>{dispatch(remove({id:key.id}))}}>Remove</button>
+                  </td>
              </tr>
             </>
         )
@@ -36,6 +51,7 @@ const MyCart=()=>{
           {ans}
         </tbody>
         </Table>
+       
         </>
     )
 }
