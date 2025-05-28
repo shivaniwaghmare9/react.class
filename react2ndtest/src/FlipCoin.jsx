a4a7f7405300166dc8a7766db94671386d0e03da
shivani <shivaniw69@gmail.com>
2025-05-28 10:31:42 +0530

    css

--- react2ndtest/src/FlipCoin.jsx
+++ react2ndtest/src/FlipCoin.jsx
@@ -1,5 +1,6 @@
 
 import { useState } from "react";
+// import img1 from "./coin.jpg";
 const FlipCoin=()=>{
     const [coin,setCoin]=useState("Heads");
     const [flipcount,setFlipcount]=useState(0);
@@ -23,13 +24,17 @@ const FlipCoin=()=>{
 
     return(
         <>
-        <div>
+        <div id="div">
           <h1>COIN FLIP TASK!! </h1>
-          <h2>Start: {coin}</h2>
-          <h2>Flip Coin Count : {flipcount}</h2>
-          <h2>Counting of Heads :{heads}</h2>
-          <h2>Counting of Tails : {tails}</h2>
-          <button onClick={flipcoin}>Flip Me!!</button>
+          <div className="box">
+          <h3> {coin}</h3>
+          </div>
+          <div className="counter">
+          <h3 id="add">Flip Coin Count : {flipcount}</h3>
+          <h3 id="heads">Counting of Heads :{heads}</h3>
+          <h3 id="tails">Counting of Tails : {tails}</h3>
+          </div>
+          <button onClick={flipcoin} id="btn">Flip Me!!</button>
         </div>
           
         </>
