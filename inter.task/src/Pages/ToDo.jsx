088c57c3294fb570c0e53177f6abe5f7b0ff58ab
shivani <shivaniw69@gmail.com>
2025-05-29 16:27:31 +0530

    complete

--- inter.task/src/Pages/ToDo.jsx
+++ inter.task/src/Pages/ToDo.jsx
@@ -1,6 +1,6 @@
 
 import { useSelector,useDispatch } from "react-redux";
-import { addTask,Deletetask } from "../TodoSlice";
+import { addTask,Deletetask,DeleteByIndex,Complete,InComplete } from "../TodoSlice";
 import { useState } from "react";
 import Table from 'react-bootstrap/Table';
 
@@ -9,15 +9,35 @@ const ToDo=()=>{
     const dispatch=useDispatch();
     const[val,setVal]=useState("");
     let sno=0;
-    const show=data.map((key)=>{
+    const show=data.map((key,index)=>{
         sno++;
         return(
             <>
              <tr>
                 <td>{sno}</td>
-                <td>{key.work}</td>
+                <td>{key.taskStatus?(
+                       <>
+                       <span style={{color:"red",textDecoration:"line-through"}}>
+                           {key.work}
+                       </span>
+                       </>
+                    ):(
+                       <>
+                        {key.work}
+                       </>
+                   )}
+                </td>
                 <td>
                     <button onClick={()=>{dispatch(Deletetask({id:key.id}))}}>Delete</button>
+                </td>
+                <td>
+                    <button onClick={()=>{dispatch(DeleteByIndex({id:index}))}}>Remove</button>
+                </td>
+                <td>
+                    <button onClick={()=>{dispatch(Complete({id:key.id}))}}>Complete</button>
+                </td>
+                <td>
+                    <button onClick={()=>{dispatch(InComplete({id:key.id}))}}>InComplete</button>
                 </td>
              </tr>
             </>
@@ -28,14 +48,16 @@ const ToDo=()=>{
         <h1>Todo App!!!</h1>
         Enter task: <input type="text"value={val} 
         onChange={(e)=>{setVal(e.target.value)}}/><br/><br/>
-        <button onClick={()=>{dispatch(addTask({id:Date.now(),work:val}))}}>Add!!!</button>
+        <button onClick={()=>{dispatch(addTask({id:Date.now(),work:val,taskStatus:false}))}}>Add!!!</button>
         <Table striped bordered hover>
       <thead>
         <tr>
           <th>Sno</th>
           <th>Task</th>
           <th>Delete</th>
-          <th>Username</th>
+          <th>Remove</th>
+          <th>Complete</th>
+          <th>Incomplete</th>
         </tr>
       </thead>
       <tbody>
--- inter.task/src/todoSlice.jsx
+++ inter.task/src/todoSlice.jsx
@@ -10,10 +10,41 @@ const todoSlice=createSlice({
             state.task.push(actions.payload)
         },
         Deletetask:(state,actions)=>{
-             console.log(actions.payload)
-            state.task=state.task.filter(item=>item.id!=actions.payload.id);
+            //  console.log(actions.payload)
+            // state.task=state.task.filter(item=>item.id!=actions.payload.id);
+            state.task=state.task.filter(function(key){
+                if(key.id==actions.payload.id)
+                {
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            })
+        },
+        DeleteByIndex:(state,actions)=>{
+            state.task.splice(actions.payload.id,1);
+            console.log(actions.payload.id)
+        },
+        Complete:(state,actions)=>{
+            for(var i=0; i<state.task.length;i++)
+            {
+                if(state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].taskStatus=true;
+                }
+            }
+        },
+        InComplete:(state,actions)=>{
+            for(var i=0; i<state.task.length;i++)
+            {
+                if(state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].taskStatus=false;
+                }
+            }
         }
     }
 })
-export const{addTask,Deletetask}=todoSlice.actions;
+export const{addTask,Deletetask,DeleteByIndex,Complete,InComplete}=todoSlice.actions;
 export default todoSlice.reducer;
