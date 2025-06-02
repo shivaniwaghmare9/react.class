93c8b2690e9225fe2df2d2dcc2ba8a6e0026a82b
shivani <shivaniw69@gmail.com>
2025-06-02 18:24:17 +0530

    evnet

--- bootapp/src/App.jsx
+++ bootapp/src/App.jsx
@@ -1,27 +1,140 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Service from "./pages/Services";
-import Join from "./pages/Join";
-import Contact from "./pages/Contact";
+
+
+
+// const App=()=>{
+//   const myname=(nm)=>{              //parameter
+//     alert("Hello i am "+ nm + " From Bhopal")
+//   }
+//   // function Display(){
+//   //   alert("Welcome to react Events!!")
+//   // }
+//   return(
+//     <>
+//      <h1>Welcome To Event App</h1>
+//      {/* <button onClick={Display}>Click Here</button> */}
+//      <button onClick={()=>{myname("Shivani")}}>Click Here</button>
+//     </>
+//   )
+// }
+// export default App;
+
+//===============================================PARAMETER================================================================================================
+
+// const App=()=>{
+//   const myname=(nm)=>{              //parameter
+//     alert("Hello i am "+ nm + " From Bhopal")
+//   }
+  
+//   return(
+//     <>
+//      <h1>Welcome To Event App</h1>
+
+//      <button onClick={()=>{myname("Shivani")}}>Click Here</button>
+//     </>
+//   )
+// }
+// export default App;
+//==============================================(double parameter)=======================================================================================
+
+// const App=()=>{
+//   const MyName=(nm,frm)=>{              //parameter
+//     alert(`Welcome ${frm} ${nm} we are from bhopal`)
+//   }
+  
+//   return(
+//     <>
+//      <h1>Welcome To Event App</h1>
+
+//      <button onClick={()=>{MyName("Shivani","Waghmare")}}>Click Here</button>
+//     </>
+//   )
+// }
+// export default App;
+
+//=====================================================================================================================================================
+
+// const App=()=>{
+//  const Display=()=>{
+//   // alert(`Welcome`)
+//   console.log(e.type)
+//  }
+  
+//   return(
+//     <>
+//      <h3>Welcome To Event App</h3>
+
+//      <button onClick={Display}>Click Here</button>
+//     </>
+//   )
+// }
+// export default App;
+//=============================================================================================================================
+
+// const App=()=>{
+//  const MyName=(nm,e)=>{
+//   alert(`Myname:  ${nm} Type: ${e.type}`)
+//  }
+  
+//   return(
+//     <>
+//      <h3>Welcome To Event App</h3>
+
+//      <button onClick={(e)=>{MyName("Shivani",e)}}>Click Here</button>
+//     </>
+//   )
+// }
+// export default App;
+
+//=====================================================================================================================
+
+// const App=()=>{
+//  const display=(e)=>{
+//   let nm=e.target.name;
+//   let val=e.target.value;
+//   alert(`Name: ${nm} Value: ${val}`);
+//  }
+  
+//   return(
+//     <>
+//      <h3>Welcome To Event App</h3>
+
+//      <button name="Mybtn1" value="BTNval" onClick={display}>Click Here</button>
+//     </>
+//   )
+// }
+// export default App;
+//====================================================================================================================================================================
+
+// const App=()=>{
+//  const handleInput=(e)=>{
+//   let name=e.target.name;
+//   let value=e.target.value;
+//   console.log(name,value)
+  
+//  }
+  
+//   return(
+//     <>
+//      <h3>Welcome To Event App!!!</h3>
+//      Enter City: <input type="text"  name="myCity" value="Bhopal" onChange={handleInput}/>
+//     </>
+//   )
+// }
+// export default App;
+//================================================================================================================================
 
 const App=()=>{
+ const handleInput=(e)=>{
+  let name=e.target.name;
+  let value=e.target.value;
+  console.log({[name]:value})
+  
+ }
+  
   return(
     <>
-     <BrowserRouter>
-       <Routes>
-        <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="home" element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="service" element={<Service/>}/>
-        <Route path="join" element={<Join/>}/>
-        <Route path="contact" element={<Contact/>}/>
-
-        </Route>
-       </Routes>
-     </BrowserRouter>
+     <h3>Welcome To Event App!!!</h3>
+     Enter City: <input type="text"  name="city" value="Indore" onChange={handleInput}/>
     </>
   )
 }
