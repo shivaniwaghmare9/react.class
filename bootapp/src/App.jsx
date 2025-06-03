2aac134f1191af0f7607d16f1d9f0d20975d13cd
shivani <shivaniw69@gmail.com>
2025-06-03 18:24:31 +0530

    hooks

--- bootapp/src/App.jsx
+++ bootapp/src/App.jsx
@@ -1,141 +1,48 @@
 
-
-
+// import { useState } from "react";
 // const App=()=>{
-//   const myname=(nm)=>{              //parameter
-//     alert("Hello i am "+ nm + " From Bhopal")
+//   const[count,setCount]=useState(0)
+//   const incre=()=>{
+//     setCount(count+1);
 //   }
-//   // function Display(){
-//   //   alert("Welcome to react Events!!")
-//   // }
-//   return(
-//     <>
-//      <h1>Welcome To Event App</h1>
-//      {/* <button onClick={Display}>Click Here</button> */}
-//      <button onClick={()=>{myname("Shivani")}}>Click Here</button>
-//     </>
-//   )
-// }
-// export default App;
-
-//===============================================PARAMETER================================================================================================
-
-// const App=()=>{
-//   const myname=(nm)=>{              //parameter
-//     alert("Hello i am "+ nm + " From Bhopal")
+//   const myDec=()=>{
+//     if(count==0)
+//     {
+//       alert("No decrement")
+//     }
+//     else{
+//       setCount(count-1);
+//     }
+    
 //   }
-  
 //   return(
 //     <>
-//      <h1>Welcome To Event App</h1>
-
-//      <button onClick={()=>{myname("Shivani")}}>Click Here</button>
-//     </>
-//   )
-// }
-// export default App;
-//==============================================(double parameter)=======================================================================================
-
-// const App=()=>{
-//   const MyName=(nm,frm)=>{              //parameter
-//     alert(`Welcome ${frm} ${nm} we are from bhopal`)
-//   }
-  
-//   return(
-//     <>
-//      <h1>Welcome To Event App</h1>
-
-//      <button onClick={()=>{MyName("Shivani","Waghmare")}}>Click Here</button>
+//     <div>
+//      <h3>Counter App!</h3>
+//      <button onClick={incre}>Increment</button>
+//      <h2>{count}</h2>
+//      <button onClick={myDec}>Deccrement</button>
+//      <button onClick={()=>{setCount(0)}}>Reset</button>
+//      </div>
 //     </>
 //   )
 // }
 // export default App;
 
-//=====================================================================================================================================================
-
-// const App=()=>{
-//  const Display=()=>{
-//   // alert(`Welcome`)
-//   console.log(e.type)
-//  }
-  
-//   return(
-//     <>
-//      <h3>Welcome To Event App</h3>
-
-//      <button onClick={Display}>Click Here</button>
-//     </>
-//   )
-// }
-// export default App;
-//=============================================================================================================================
-
-// const App=()=>{
-//  const MyName=(nm,e)=>{
-//   alert(`Myname:  ${nm} Type: ${e.type}`)
-//  }
-  
-//   return(
-//     <>
-//      <h3>Welcome To Event App</h3>
-
-//      <button onClick={(e)=>{MyName("Shivani",e)}}>Click Here</button>
-//     </>
-//   )
-// }
-// export default App;
-
-//=====================================================================================================================
-
-// const App=()=>{
-//  const display=(e)=>{
-//   let nm=e.target.name;
-//   let val=e.target.value;
-//   alert(`Name: ${nm} Value: ${val}`);
-//  }
-  
-//   return(
-//     <>
-//      <h3>Welcome To Event App</h3>
-
-//      <button name="Mybtn1" value="BTNval" onClick={display}>Click Here</button>
-//     </>
-//   )
-// }
-// export default App;
-//====================================================================================================================================================================
-
-// const App=()=>{
-//  const handleInput=(e)=>{
-//   let name=e.target.name;
-//   let value=e.target.value;
-//   console.log(name,value)
-  
-//  }
-  
-//   return(
-//     <>
-//      <h3>Welcome To Event App!!!</h3>
-//      Enter City: <input type="text"  name="myCity" value="Bhopal" onChange={handleInput}/>
-//     </>
-//   )
-// }
-// export default App;
-//================================================================================================================================
-
+//=================================================================================================================================================
+import { useState,useEffect } from "react";
 const App=()=>{
- const handleInput=(e)=>{
-  let name=e.target.name;
-  let value=e.target.value;
-  console.log({[name]:value})
-  
- }
-  
+  const[myval,setMyval]=useState(0);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setMyval(myval+1);
+    },3000)
+  })
   return(
     <>
-     <h3>Welcome To Event App!!!</h3>
-     Enter City: <input type="text"  name="city" value="Indore" onChange={handleInput}/>
+    <h3>welcome to my App:{myval}</h3>
     </>
+
   )
 }
 export default App;
