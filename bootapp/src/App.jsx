6a924f57ed5f53a54afb76c7b96d199ebb51f4ff
shivani <shivaniw69@gmail.com>
2025-06-04 22:21:14 +0530

    App

--- bootapp/src/App.jsx
+++ bootapp/src/App.jsx
@@ -1,48 +1,59 @@
 
-// import { useState } from "react";
+
+
+// //===========================================useeffect(1)======================================================================================================
+// import { useState,useEffect } from "react";
 // const App=()=>{
-//   const[count,setCount]=useState(0)
-//   const incre=()=>{
-//     setCount(count+1);
-//   }
-//   const myDec=()=>{
-//     if(count==0)
-//     {
-//       alert("No decrement")
-//     }
-//     else{
-//       setCount(count-1);
-//     }
-    
-//   }
+//   const[myval,setMyval]=useState(0);
+//   useEffect(()=>{
+//     setTimeout(()=>{
+//       setMyval(myval+1);
+//     },3000)
+//   })
 //   return(
 //     <>
-//     <div>
-//      <h3>Counter App!</h3>
-//      <button onClick={incre}>Increment</button>
-//      <h2>{count}</h2>
-//      <button onClick={myDec}>Deccrement</button>
-//      <button onClick={()=>{setCount(0)}}>Reset</button>
-//      </div>
+//     <h3>welcome to my App:{myval}</h3>
 //     </>
+
 //   )
 // }
 // export default App;
 
-//=================================================================================================================================================
+//===========================================================================================================================
+
+//===========================================useeffect(2)======================================================================================================
+// import { useState,useEffect } from "react";
+// const App=()=>{
+//   const[myval,setMyval]=useState(0);
+//   useEffect(()=>{
+//     setTimeout(()=>{
+//       setMyval(myval+1);
+//     },3000)
+//   },[])
+//   return(
+//     <>
+//     <h3>welcome to my App:{myval}</h3>
+//     </>
+
+//   )
+// }
+// export default App;
+//===============================================useeffect(3)==========================================================================================
+
 import { useState,useEffect } from "react";
 const App=()=>{
-  const[myval,setMyval]=useState(0);
+  const [count,setCount]=useState(0);
+  const[multi,setMulti]=useState(0);
   useEffect(()=>{
-    setTimeout(()=>{
-      setMyval(myval+1);
-    },3000)
-  })
+    setMulti(count*2)
+  },[count])
   return(
-    <>
-    <h3>welcome to my App:{myval}</h3>
-    </>
-
+  <>
+  <h3>Counter App!!</h3>
+  <h4>Addition: {count}</h4>
+  <h4>Multiplication:{multi}</h4>
+  <button onClick={()=>{setCount(count+1)}}>Click me</button>
+  </>
   )
 }
 export default App;
