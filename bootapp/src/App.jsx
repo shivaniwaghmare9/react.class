2b880d5df263c5ac5b657587c43b43f72c8e8341
shivani <shivaniw69@gmail.com>
2025-06-05 22:56:10 +0530

    emp

--- bootapp/src/App.jsx
+++ bootapp/src/App.jsx
@@ -1,3 +1,37 @@
+
+// import axios from "axios";
+// import { useState } from "react";
+// const App=()=>{
+//   const[input,setInput]=useState({});
+//   const handleInput=(e)=>{
+//     let name=e.target.name;
+//     let value=e.target.value;
+//     setInput(Values=>({...Values,[name]:value}))
+//     console.log(input)
+//   }
+//   const handleSubmit=async()=>{
+//     // console.log(input)
+//     let api="http://localhost:3000/students";
+//     const response=await axios.post(api,input);
+//     console.log(response.data)
+//     alert("Data Inserted")
+//   }
+
+//   return(
+//     <>
+//      <h3>Application Form!!</h3>
+//     Enter Name: <input type="text" name="name"  onChange={handleInput}/><br/><br/>
+//     Enter Rollno: <input type="text" name="rollno"  onChange={handleInput}/><br/><br/>
+//     Enter City: <input type="text" name="city"  onChange={handleInput}/><br/><br/>
+//     Enter Fees: <input type="text" name="fees"  onChange={handleInput}/><br/><br/>
+//     <button onClick={handleSubmit}>Save!!!</button>
+//     </>
+//   )
+// }
+// export default App;
+
+
+import axios from "axios";
 import { useState } from "react";
 const App=()=>{
   const[input,setInput]=useState({});
@@ -7,16 +41,21 @@ const App=()=>{
     setInput(Values=>({...Values,[name]:value}))
     console.log(input)
   }
-  const handleSubmit=()=>{
-    console.log(input)
+  const handleSubmit=async()=>{
+    // console.log(input)
+    let api="http://localhost:3000/employee";
+    const response=await axios.post(api,input);
+    console.log(response.data)
+    alert("Data Inserted")
   }
+
   return(
     <>
-     <h3>Application Form!!</h3>
-    Enter Name: <input type="text" name="name"  onChange={handleInput}/><br/><br/>
-    Enter Rollno: <input type="text" name="rollno"  onChange={handleInput}/><br/><br/>
+     <h3>Employee Records!!</h3>
+    Employee name: <input type="text" name="name"  onChange={handleInput}/><br/><br/>
+    Employee Number: <input type="text" name="number"  onChange={handleInput}/><br/><br/>
+    Enter Designation: <input type="text" name="designation"  onChange={handleInput}/><br/><br/>
     Enter City: <input type="text" name="city"  onChange={handleInput}/><br/><br/>
-    Enter Fees: <input type="text" name="fees"  onChange={handleInput}/><br/><br/>
     <button onClick={handleSubmit}>Save!!!</button>
     </>
   )
