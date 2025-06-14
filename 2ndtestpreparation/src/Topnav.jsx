2d693544e21ed2944e6825fe20eb699718908b4e
shivani <shivaniw69@gmail.com>
2025-06-14 22:17:12 +0530

    top

--- 2ndtestpreparation/src/Topnav.jsx
+++ 2ndtestpreparation/src/Topnav.jsx
@@ -1,5 +1,33 @@
 
 
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { Link } from 'react-router-dom';
+// const Topnav=()=>{
+//     return(
+//         <>
+
+// <Navbar bg="dark" data-bs-theme="dark">
+//         <Container>
+//           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+//           <Nav className="me-auto">
+//             <Nav.Link as={Link} to="home">Home</Nav.Link>
+//             <Nav.Link as={Link} to="about">About</Nav.Link>
+//             <Nav.Link as={Link} to="faculty">Faculty</Nav.Link>
+//             <Nav.Link as={Link} to="course">OurCourse</Nav.Link>
+//             <Nav.Link as={Link} to="contact">Contact Us</Nav.Link>
+            
+//           </Nav>
+//         </Container>
+//       </Navbar>
+//         </>
+//     )
+// }
+// export default Topnav;
+
+
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
