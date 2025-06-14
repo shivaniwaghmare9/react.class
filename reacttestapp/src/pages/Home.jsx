36399ee7106dab2d424eaab3946cd0aaa7c473cd
shivani <shivaniw69@gmail.com>
2025-06-14 22:19:34 +0530

    tohead

--- reacttestapp/src/pages/Home.jsx
+++ reacttestapp/src/pages/Home.jsx
@@ -14,3 +14,18 @@ const Home=()=>{
     )
 }
 export default Home;
+
+// const App=()=>{
+//   return(
+//     <>
+//      <div style={{width:"300px",height:"200px",border:"2px solid black",marginLeft:"30%",backgroundColor:"blue",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:"20px"}}>
+//        <div style={{width:"250px",height:"150px",border:"2px solid black",backgroundColor:"yellow",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:"20px"}}>
+//           <div style={{width:"200px",height:"100px",border:"2px solid black",backgroundColor:"red",borderRadius:"20px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+//            <h3>CYBROM</h3>
+//      </div>
+//      </div>
+//      </div>
+//     </>
+//   )
+// }
+// export default App;
