35f9d6aeffcc74692013a516926550cedb8f7ca9
shivani <shivaniw69@gmail.com>
2025-06-14 22:16:29 +0530

    style

--- 2ndtestpreparation/src/ColorSlice.jsx
+++ 2ndtestpreparation/src/ColorSlice.jsx
@@ -1,21 +1,4 @@
 
-// import { createSlice } from "@reduxjs/toolkit"
-// const ColorSlice=createSlice({
-//     name:"mycolor",
-//     initialState:{
-//         color:"blue"
-//     },
-//     reducers:{
-//         ChangeColor:(state,actions)=>{
-//             console.log(actions.payload)
-//             state.color=(actions.payload)
-//         }
-//     }
-// })
-// export const{ChangeColor}=ColorSlice.actions;
-// export default ColorSlice.reducer;
-
-
 import { createSlice } from "@reduxjs/toolkit"
 const ColorSlice=createSlice({
     name:"mycolor",
