5feee1f7674efdae476395fcf6f0c99523c25cee
shivani <shivaniw69@gmail.com>
2025-06-14 21:59:24 +0530

    new

--- classApp5pm/src/LoginContext.jsx
+++ classApp5pm/src/LoginContext.jsx
@@ -1,4 +1,26 @@
 
+// import { createContext,useState } from "react";
+// const MyContext=createContext();
+// const LoginContext=({children})=>{
+//     const[user,setUser]=useState({name:"",auth:false})
+//     const login=(nm)=>{
+//         setUser({name:nm, auth:true})
+//     }
+//     const logout=()=>{
+//         setUser({name:"", auth:false})
+//     }
+//     return(
+//         <>
+//         <MyContext.Provider value={{user,login,logout}}>
+//            {children}
+//         </MyContext.Provider>
+//         </>
+//     )
+// }
+// export default LoginContext;  
+// export {MyContext}
+
+
 import { createContext,useState } from "react";
 const MyContext=createContext();
 const LoginContext=({children})=>{
