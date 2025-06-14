235a642ee657d89bc4f383d53780efe132f3eacb
shivani <shivaniw69@gmail.com>
2025-06-14 22:00:23 +0530

    App

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -1,4 +1,20 @@
 
+// import { useContext } from "react";
+// import Auth from "./Auth";
+// import Unauth from "./Unauth";
+// import { MyContext } from "./LoginContext";
+// const App=()=>{
+//     const{user}=useContext(MyContext)
+//     return(
+//         <>
+//         <h1>Login App!!</h1>
+//          {user.auth ? <Auth/>:<Unauth/>}
+//         </>
+//     )
+// }
+// export default App;
+
+
 import { useContext } from "react";
 import Auth from "./Auth";
 import Unauth from "./Unauth";
@@ -13,3 +29,5 @@ const App=()=>{
     )
 }
 export default App;
+
+
