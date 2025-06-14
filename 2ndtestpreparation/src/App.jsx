4e6f97f3be871f908dd95af7889410cea9658705
shivani <shivaniw69@gmail.com>
2025-06-14 22:01:21 +0530

    App

--- 2ndtestpreparation/src/App.jsx
+++ 2ndtestpreparation/src/App.jsx
@@ -1,10 +1,40 @@
 
+// import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import Layout from "./Layout";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Faculty from "./pages/Faculty";
+// import Course from "./pages/OurCourse";
+// import Contact from "./pages/Contact";
+// const App=()=>{
+//   return(
+//     <>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout/>}>
+//         <Route index element={<Home/>}/>
+//         <Route path="home" element={<Home/>}/>
+//         <Route path="about" element={<About/>}/>
+//         <Route path="faculty" element={<Faculty/>}/>
+//         <Route path="course" element={<Course/>}/>
+//         <Route path="contact" element={<Contact/>}/>
+//         </Route>
+//       </Routes>
+
+//     </BrowserRouter>
+//     </>
+//   )
+// }
+// export default App;
+
+
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faculty from "./pages/Faculty";
 import Course from "./pages/OurCourse";
+import Contact from "./pages/Contact";
 const App=()=>{
   return(
     <>
@@ -16,6 +46,7 @@ const App=()=>{
         <Route path="about" element={<About/>}/>
         <Route path="faculty" element={<Faculty/>}/>
         <Route path="course" element={<Course/>}/>
+        <Route path="contact" element={<Contact/>}/>
         </Route>
       </Routes>
 
---
+++ 2ndtestpreparation/src/pages/Contact.jsx
@@ -0,0 +1,20 @@
+
+
+const Contact=()=>{
+    return(
+        <>
+        <div className="form">
+           <h3>Student Records Form!!!</h3>
+           <input type="text" name="name" placeholder="Name"/><br/><br/>
+           <input type="text" name="rollno" placeholder="Rollno"/><br/><br/>
+           <input type="number" name="physics" placeholder="Physics"/><br/><br/>
+           <input type="number" name="chemistry" placeholder="Chemistry"/><br/><br/>
+           <input type="number" name="maths" placeholder="Maths"/><br/><br/>
+           <input type="number" name="english" placeholder="English"/><br/><br/>
+           <input type="number" name="hindi" placeholder="Hindi"/><br/><br/>
+           <button>Calculate</button>
+        </div>
+        </>
+    )
+}
+export default Contact;
