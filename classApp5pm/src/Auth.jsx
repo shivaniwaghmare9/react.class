adb7c5bbd6e86cfc0bafce0e33b96e65a44de51a
shivani <shivaniw69@gmail.com>
2025-06-14 21:59:56 +0530

    new

--- classApp5pm/src/Auth.jsx
+++ classApp5pm/src/Auth.jsx
@@ -1,4 +1,18 @@
 
+// import { useContext } from "react";
+// import { MyContext } from "./LoginContext";
+// const Auth=()=>{
+//     const{user,logout}=useContext(MyContext);
+//     return(
+//         <>
+//          <h2>Welcome to :{user.name}</h2>
+//          <button onClick={logout}>Logout</button>
+//         </>
+//     )
+// }
+// export default Auth;
+
+
 import { useContext } from "react";
 import { MyContext } from "./LoginContext";
 const Auth=()=>{
