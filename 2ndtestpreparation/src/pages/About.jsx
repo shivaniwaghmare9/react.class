e4a5be5fd454979c7eef4fad6fd2b49845b18078
shivani <shivaniw69@gmail.com>
2025-06-14 22:02:45 +0530

    faculty

--- 2ndtestpreparation/src/pages/About.jsx
+++ 2ndtestpreparation/src/pages/About.jsx
@@ -1,35 +1,4 @@
 
-// import axios from "axios";
-// import { useState } from "react";
-// const About=()=>{
-//     const[input,setInput]=useState({});
-//     const handleInput=(e)=>{
-//         let name=e.target.name;
-//         let value=e.target.value;
-//         setInput(Values=>({...Values,[name]:value}));
-//         console.log(input);
-//     }
-//     const handleSubmit=async()=>{
-//         let api="http://localhost:3000/student";
-//         const response=await axios.post(api,input);
-//         alert("data successfully inserted!!")
-//         console.log(response.data);
-//     }
-//     return(
-//         <>
-//         <form id="form">
-//             Enter Name: <input type="text" name="name" onChange={handleInput}/><br/><br/>
-//             Enter Rollno: <input type="text" name="rollno" onChange={handleInput}/><br/><br/>
-//             Enter City: <input type="text" name="city" onChange={handleInput}/><br/><br/>
-//             Enter Fees: <input type="text" name="fees" onChange={handleInput}/><br/><br/>
-//             <button onClick={handleSubmit}>Submit</button>
-//         </form>
-//         </>
-//     )
-// }
-// export default About;
-
-
 import axios from "axios";
 import { useState } from "react";
 const About=()=>{
--- 2ndtestpreparation/src/pages/Faculty.jsx
+++ 2ndtestpreparation/src/pages/Faculty.jsx
@@ -1,4 +1,35 @@
 
+// import { useState } from "react";
+// const Faculty=()=>{
+//     const[count,setCount]=useState(0);
+//     const Incre=()=>{
+//        setCount(count+1);
+//     }
+//     const Decre=()=>{
+//         if(count==0)
+//         {
+//             alert("not less then 0")
+//         }
+//         else
+//         {
+//             setCount(count-1);  
+//         }
+//     }
+//     return(
+//         <>
+//        <div className="counter">
+//            <h1>Counter App!!!</h1>
+//            <h2>{count}</h2>
+//            <button onClick={Incre} className="btn1">Incremnet</button>
+//            <button onClick={Decre} className="btn2">Decrement</button>
+//            <button onClick={()=>{setCount(0)}} className="btn3">Reset</button>
+//        </div>
+//         </>
+//     )
+// }
+// export default Faculty; 
+
+
 import { useState } from "react";
 const Faculty=()=>{
     const[count,setCount]=useState(0);
