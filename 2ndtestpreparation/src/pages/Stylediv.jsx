34b01aa6c9efb0a0b4e59fbef1e3deb3298f0ca2
shivani <shivaniw69@gmail.com>
2025-06-14 22:28:11 +0530

    change

--- 2ndtestpreparation/src/pages/Stylediv.jsx
+++ 2ndtestpreparation/src/pages/Stylediv.jsx
@@ -3,7 +3,7 @@
  const Style=()=>{
   return(
     <>
-     <div style={{width:"300px",height:"200px",border:"2px solid black",marginLeft:"30%",backgroundColor:"blue",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:"20px"}}>
+     <div style={{width:"300px",height:"200px",border:"2px solid black",marginLeft:"35%",backgroundColor:"blue",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:"20px",marginTop:"10%"}}>
        <div style={{width:"250px",height:"150px",border:"2px solid black",backgroundColor:"yellow",display:"flex",justifyContent:"center",alignItems:"center",borderRadius:"20px"}}>
           <div style={{width:"200px",height:"100px",border:"2px solid black",backgroundColor:"red",borderRadius:"20px",display:"flex",justifyContent:"center",alignItems:"center"}}>
            <h3>CYBROM</h3>
