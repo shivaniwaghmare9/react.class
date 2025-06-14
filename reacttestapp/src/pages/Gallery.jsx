15ba58d5bdbc6d23bec761e98dc586d4581ace54
shivani <shivaniw69@gmail.com>
2025-06-14 22:21:20 +0530

    gallary

--- reacttestapp/src/pages/Gallery.jsx
+++ reacttestapp/src/pages/Gallery.jsx
@@ -1,4 +1,27 @@
 
+// import { useState,useEffect } from "react";
+// const Gallery=()=>{
+//     const[count,setCount]=useState(0);
+//     const[multi,setMulti]=useState(0);
+//     useEffect(()=>{
+//         setMulti(count*2)
+//     },[count])
+    
+        
+//     return(
+//         <>
+//         <div className="box2">
+//          <h1>Counter:{count}</h1>
+//          <h1 >Multiplication:{multi}</h1>
+//          <button onClick={()=>{setCount(count+1)}} className="btn3">Click</button>
+//          </div>
+         
+//         </>
+//     )
+// }
+// export default Gallery;
+
+
 import { useState,useEffect } from "react";
 const Gallery=()=>{
     const[count,setCount]=useState(0);
