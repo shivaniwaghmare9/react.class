d6f81048383bf4e272e2e88a48ab6d22b0224e8b
shivani <shivaniw69@gmail.com>
2025-06-14 22:18:38 +0530

    layout

--- 2ndtestpreparation/src/Layout.jsx
+++ 2ndtestpreparation/src/Layout.jsx
@@ -1,4 +1,18 @@
 
+// import { Outlet } from "react-router-dom"
+// import Topnav from "./Topnav";
+// const Layout=()=>{
+//     return(
+//         <>
+//         <Topnav/>
+//         <Outlet/>
+
+//         </>
+//     )
+// }
+// export default Layout;
+
+
 import { Outlet } from "react-router-dom"
 import Topnav from "./Topnav";
 const Layout=()=>{
--- 2ndtestpreparation/src/store.jsx
+++ 2ndtestpreparation/src/store.jsx
@@ -1,12 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import ColorReducer from "./ColorSlice"
-// const store=configureStore({
-//     reducer:{
-//          mycolor:ColorReducer
-//     }
-// })
-// export default store;
-
 import { configureStore } from "@reduxjs/toolkit";
 import ColorReducer from "./ColorSlice"
 const store=configureStore({
