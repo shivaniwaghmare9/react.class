22896ca20be4f49a616a79847b6710ae8f93baae
shivani <shivaniw69@gmail.com>
2025-06-14 22:18:01 +0530

    store

--- 2ndtestpreparation/src/store.jsx
+++ 2ndtestpreparation/src/store.jsx
@@ -1,3 +1,12 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import ColorReducer from "./ColorSlice"
+// const store=configureStore({
+//     reducer:{
+//          mycolor:ColorReducer
+//     }
+// })
+// export default store;
+
 import { configureStore } from "@reduxjs/toolkit";
 import ColorReducer from "./ColorSlice"
 const store=configureStore({
