9a3c831b6d42af4ed4a71814ea4d0522411a7646
shivani <shivaniw69@gmail.com>
2025-06-15 22:38:23 +0530

    table

---
+++ classApp5pm/src/pages/Display.jsx
@@ -0,0 +1,46 @@
+
+import axios from "axios";
+import { useState,useEffect } from "react";
+const Display=()=>{
+    const[mydata,setMydata]=useState([]);
+    const loadData=async()=>{
+        let api="http://localhost:3000/record";
+        const response=await axios.get(api);
+        console.log(response.data)
+        setMydata(response.data)
+    }
+    useEffect(()=>{
+       loadData();
+    },[])
+    let sno=0;
+    const ans=mydata.map((key)=>{
+        sno++;
+        return(
+            <>
+             <tr>
+                <td>{sno}</td>
+                <td>{key.name}</td>
+                <td>{key.rollno}</td>
+                <td>{key.city}</td>
+                <td>{key.fees}</td>
+             </tr>
+            </>
+        )
+    })
+    return(
+        <>
+        <h1>Display Data!!!</h1><br/><br/>
+        <table>
+            <tr>
+                <th>Sno</th>
+                <th>Name</th>
+                <th>Rollno</th>
+                <th>City</th>
+                <th>Fees</th>
+            </tr>
+            {ans}
+        </table>
+        </>
+    )
+}
+export default Display;
