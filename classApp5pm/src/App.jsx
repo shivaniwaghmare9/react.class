0df79ceb469f0b38f5802716164f15f1307fe91f
shivani <shivaniw69@gmail.com>
2025-06-15 21:57:58 +0530

    app

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -1,18 +1,22 @@
 
 import { useState } from "react";
 const App=()=>{
-    const[name,setName]=useState("");
-    const[city,setCity]=useState("");
-
-    const handleSubmit=()=>{
-        alert(`Name : ${name} City: ${city}`)
+    const[input,setInput]=useState({});
+    const handleInput=(e)=>{
+        let name=e.target.name;
+        let value=e.target.value;
+        setInput(Values=>({...Values,[name]:value}))
+        console.log(input)
     }
+    
     return(
         <>
           <h1>Welcome to useState hook</h1>
-          Enter Name : <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/><br/><br/>
-          Enter City : <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}}/><br/><br/>
-          <button onClick={handleSubmit}>Click</button>
+          Enter Name : <input type="text" name="name" onChange={handleInput}/><br/><br/>
+          Enter City : <input type="text" name="city" onChange={handleInput}/><br/><br/>
+          Enter Roll : <input type="text" name="roll" onChange={handleInput}/><br/><br/>
+          Enter Fees : <input type="text" name="fees" onChange={handleInput}/><br/><br/>
+          <button onClick={handleInput}>Click</button>
 
         </>
     )
