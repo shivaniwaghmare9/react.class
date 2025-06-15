158cffdb155f68b640a579803f20287946779d34
shivani <shivaniw69@gmail.com>
2025-06-15 21:46:57 +0530

    handle

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -1,14 +1,19 @@
 
 import { useState } from "react";
 const App=()=>{
-    const [color,setColor]=useState("red");
+    const[name,setName]=useState("");
+    const[city,setCity]=useState("");
+
+    const handleSubmit=()=>{
+        alert(`Name : ${name} City: ${city}`)
+    }
     return(
         <>
-        <h1 style={{color:color}}>USeState : {color}</h1>
-        <button onClick={()=>{setColor("green")}}>Green</button>
-        <button onClick={()=>{setColor("yellow")}}>Yellow</button>
-        <button onClick={()=>{setColor("blue")}}>Blue</button>
-        <button onClick={()=>{setColor("orange")}}>Oragne</button>
+          <h1>Welcome to useState hook</h1>
+          Enter Name : <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/><br/><br/>
+          Enter City : <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}}/><br/><br/>
+          <button onClick={handleSubmit}>Click</button>
+
         </>
     )
 }
