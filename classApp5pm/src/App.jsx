d3a520635453f86edc68dc10a6c7ac844bb38154
shivani <shivaniw69@gmail.com>
2025-06-15 21:28:11 +0530

    hook

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -1,16 +1,11 @@
 
+import { useState } from "react";
 const App=()=>{
-    const handleInput=(e)=>{
-       let name=e.target.name;
-       let value=e.target.value;
-       //alert(`Name: ${name} Value: ${value} Type: ${e.type}`)
-       console.log({[name]:value})
-    }
+    const [name,setName]=useState("Shivani");
     return(
         <>
-        <h1>Events!!</h1>
-      Enter Name: <input type="text" name="city" value="indore" onChange={handleInput}/><br/><br/>
-      
+        <h1>USeState : {name}</h1>
+        <button onClick={()=>{setName("Priyaa")}}>Click</button>
         </>
     )
 }
