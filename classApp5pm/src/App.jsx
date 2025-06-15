65b151ed6c84fc4ae2637a08c520b306784f60a8
shivani <shivaniw69@gmail.com>
2025-06-15 07:50:53 +0530

    useeffect

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -2,15 +2,16 @@
 import { useState,useEffect } from "react";
 const App=()=>{
     const[count,setCount]=useState(0);
+    const[multi,setMulti]=useState(0);
     useEffect(()=>{
-        setTimeout(()=>{
-        setCount(count+1)
-    },3000)
-    },[])
+        setMulti(count*2)
+    },[count])
     
     return(
         <>
         <h1>Count:{count}</h1>
+        <h1>Multi:{multi}</h1>
+        <button onClick={()=>{setCount(count+1)}}>Click</button>
         </>
     )
 }
