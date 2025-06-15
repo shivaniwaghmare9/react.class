d1dc82e1e24cbd3f81d7405f53f51263906cf3d9
shivani <shivaniw69@gmail.com>
2025-06-15 08:09:44 +0530

    somethingnew

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -1,13 +1,13 @@
 
 
 const App=()=>{
-    const show=(nm,ct)=>{
-          alert(`Hello i am  ${nm} i am from ${ct}`)
+    const show=(nm,ct,e)=>{
+          alert(`Hello i am  ${nm} i am from ${ct} Type:${e.type}`)
     }
     return(
         <>
           <h1>Events!!</h1>
-          <button onClick={()=>{show("shivani","bhopal")}}>click</button>
+          <button onClick={(e)=>{show("shivani","bhopal",e)}}>click</button>
         </>
     )
 }
