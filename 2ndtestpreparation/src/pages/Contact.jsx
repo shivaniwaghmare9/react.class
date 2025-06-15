dd72c89d2ef54e89d70e22155ce6b67b671d210b
shivani <shivaniw69@gmail.com>
2025-06-15 06:32:25 +0530

    result

--- 2ndtestpreparation/src/pages/Contact.jsx
+++ 2ndtestpreparation/src/pages/Contact.jsx
@@ -4,15 +4,15 @@ const Contact=()=>{
     const[student,setStudent]=useState({
         name:"",rollno:"",physics:"",chemistry:"",maths:"",english:"",hindi:""});
         const[result,setResult]=useState(null)
-        const handleChnage=(e)=>{
+        const handleChange=(e)=>{
             let name=e.target.name;
             let value=e.target.value;
             setStudent(Values=>({...Values,[name]:value}))
             // console.log(student);
         }
         const calculateSubmit=()=>{
-          const {phy,chem,math,eng,hin}=student;
-          const marks=[Number(phy),Number(chem),Number(math),Number(eng),Number(hin)];
+          const {physics,chemistry,maths,english,hindi}=student;
+          const marks=[Number(physics),Number(chemistry),Number(maths),Number(english),Number(hindi)];
           const total=marks.reduce((acc,mark)=>acc+mark,0)
           const percentage=total/5
 
@@ -30,7 +30,7 @@ const Contact=()=>{
 
        setResult({
         total,
-        percentage:percentage.toFixed(2),
+        percentage: percentage.toFixed(2),
         division
        })
         }
@@ -39,20 +39,20 @@ const Contact=()=>{
         <>
         <div className="form">
            <h3>Student Records Form!!!</h3>
-           <input type="text" name="name" placeholder="Name" onChange={handleChnage}/><br/><br/>
-           <input type="text" name="rollno" placeholder="Rollno"onChange={handleChnage}/><br/><br/>
-           <input type="number" name="physics" placeholder="Physics" onChange={handleChnage}/><br/><br/>
-           <input type="number" name="chemistry" placeholder="Chemistry" onChange={handleChnage}/><br/><br/>
-           <input type="number" name="maths" placeholder="Maths" onChange={handleChnage}/><br/><br/>
-           <input type="number" name="english" placeholder="English" onChange={handleChnage}/><br/><br/>
-           <input type="number" name="hindi" placeholder="Hindi" onChange={handleChnage}/><br/><br/>
+           <input type="text" name="name" placeholder="Name" onChange={handleChange}/><br/><br/>
+           <input type="text" name="rollno" placeholder="Rollno"onChange={handleChange}/><br/><br/>
+           <input type="number" name="physics" placeholder="Physics" onChange={handleChange}/><br/><br/>
+           <input type="number" name="chemistry" placeholder="Chemistry" onChange={handleChange}/><br/><br/>
+           <input type="number" name="maths" placeholder="Maths" onChange={handleChange}/><br/><br/>
+           <input type="number" name="english" placeholder="English" onChange={handleChange}/><br/><br/>
+           <input type="number" name="hindi" placeholder="Hindi" onChange={handleChange}/><br/><br/>
            <button onClick={calculateSubmit}>Calculate</button>
 
            {result && (
             <div>
-                <h3>Rsult</h3>
+                <h3>Result</h3>
                 <h4>Total Marks:{result.total}</h4>
-                <h4>Percentage:{result.percentage}</h4>
+                <h4>Percentage:{result.percentage}%</h4>
                 <h4>Division:{result.division}</h4>
             </div>
            )
