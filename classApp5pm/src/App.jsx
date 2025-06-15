bced62c7b5c693ea1ceaefbe82bd3cec63c17559
shivani <shivaniw69@gmail.com>
2025-06-15 21:17:05 +0530

    event

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -3,6 +3,7 @@ const App=()=>{
     const show=(e)=>{
        let name=e.target.name;
        let value=e.target.value;
+       alert(`Name: ${name} Value: ${value} Type: ${e.type}`)
        console.log(name,value)
     }
     return(
