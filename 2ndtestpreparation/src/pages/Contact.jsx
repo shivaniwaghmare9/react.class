f21b3c714376490511068ac5e04fea60dba1abfd
shivani <shivaniw69@gmail.com>
2025-06-15 06:15:24 +0530

    result

--- 2ndtestpreparation/src/pages/Contact.jsx
+++ 2ndtestpreparation/src/pages/Contact.jsx
@@ -4,21 +4,34 @@ const Contact=()=>{
     const[student,setStudent]=useState({
         name:"",rollno:"",physics:"",chemistry:"",maths:"",english:"",hindi:""});
         const[result,setResult]=useState(null)
-        const calculateSubmit=()=>{
-            
+        const handleChnage=(e)=>{
+            let name=e.target.name;
+            let value=e.target.value;
+            setStudent(Values=>({...Values,[name]:value}))
+            // console.log(student);
         }
-
+        const calculateSubmit=()=>{
+          const {phy,chem,math,eng,hin}=student;
+          const marks=[Number(phy),Number(chem),Number(math),Number(eng),Number(hin)];
+          const total=marks.reduce((acc,mark)=>acc+mark,0)
+          const percentage=total/5
+        }
+        let percentage=""
+        if()
+       setResult((
+        
+       ))
     return(
         <>
         <div className="form">
            <h3>Student Records Form!!!</h3>
-           <input type="text" name="name" placeholder="Name"/><br/><br/>
-           <input type="text" name="rollno" placeholder="Rollno"/><br/><br/>
-           <input type="number" name="physics" placeholder="Physics"/><br/><br/>
-           <input type="number" name="chemistry" placeholder="Chemistry"/><br/><br/>
-           <input type="number" name="maths" placeholder="Maths"/><br/><br/>
-           <input type="number" name="english" placeholder="English"/><br/><br/>
-           <input type="number" name="hindi" placeholder="Hindi"/><br/><br/>
+           <input type="text" name="name" placeholder="Name" onChange={handleChnage}/><br/><br/>
+           <input type="text" name="rollno" placeholder="Rollno"onChange={handleChnage}/><br/><br/>
+           <input type="number" name="physics" placeholder="Physics" onChange={handleChnage}/><br/><br/>
+           <input type="number" name="chemistry" placeholder="Chemistry" onChange={handleChnage}/><br/><br/>
+           <input type="number" name="maths" placeholder="Maths" onChange={handleChnage}/><br/><br/>
+           <input type="number" name="english" placeholder="English" onChange={handleChnage}/><br/><br/>
+           <input type="number" name="hindi" placeholder="Hindi" onChange={handleChnage}/><br/><br/>
            <button onClick={calculateSubmit}>Calculate</button>
         </div>
         </>
