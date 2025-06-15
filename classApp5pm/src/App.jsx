79368193a48167eb75562e18a0629d06f943db90
shivani <shivaniw69@gmail.com>
2025-06-15 07:46:10 +0530

    2ruluseeffect

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -3,8 +3,11 @@ import { useState,useEffect } from "react";
 const App=()=>{
     const[count,setCount]=useState(0);
     useEffect(()=>{
+        setTimeout(()=>{
         setCount(count+1)
-    })
+    },3000)
+    },[])
+    
     return(
         <>
         <h1>Count:{count}</h1>
