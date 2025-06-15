27c5d23893a3ffa2348b61d5a65f0ccf420f179e
shivani <shivaniw69@gmail.com>
2025-06-15 21:15:48 +0530

    app

--- classApp5pm/src/App.jsx
+++ classApp5pm/src/App.jsx
@@ -1,16 +1,14 @@
 
-
 const App=()=>{
     const show=(e)=>{
-        let name=e.target.name;
-        let value=e.target.value
-          alert(`Name ${name} Value ${value} Type ${e.type}`)
-          console.log(name,value)
+       let name=e.target.name;
+       let value=e.target.value;
+       console.log(name,value)
     }
     return(
         <>
-          <h1>Events!!</h1>
-          <button name="btn" value="mybtn" onClick={show}>click</button>
+        <h1>Events!!</h1>
+        <button name="btn" value="mybtn" onClick={show}>Click</button>
         </>
     )
 }
