e201f348d1a6fd4e854ff5799d1e4f2de2361656
shivani <shivaniw69@gmail.com>
2025-06-16 16:31:54 +0530

    unAuth

--- classApp5pm/src/UnAuth.jsx
+++ classApp5pm/src/UnAuth.jsx
@@ -2,12 +2,12 @@
 import { useContext } from "react";
 import { MyContext } from "./Logincontext";
 const UnAuth=()=>{
-   
+   const{name,login}=useContext(MyContext);
     return(
         <>
           <h1>Login System!!</h1>
-          Enter Name: <input type="text" /><br/><br/>
-          <button>Click</button>
+          Enter Name: <input type="text" value={name}/><br/><br/>
+          <button onClick={login}>Login</button>
         </>
     )
 }
