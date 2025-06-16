bdb348dd7db5000e8285d02d12a23adc4c551d0a
shivani <shivaniw69@gmail.com>
2025-06-17 01:19:38 +0530

    complte

--- homeapp/src/App.jsx
+++ homeapp/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { useSelector,useDispatch } from "react-redux";
-import { addTask,RemoveTask,ReByIndex } from "./todoSlice";
+import { addTask,RemoveTask,ReByIndex,taskComplete,taskInComplete } from "./todoSlice";
 import { useState } from "react";
 const App=()=>{ 
   const data=useSelector(state=>state.todo.task)
@@ -14,12 +14,30 @@ const App=()=>{
       <>
       <tr>
         <td>{sno}</td>
-        <td>{key.work}</td>
+        <td>{key.taskStatus?(
+          <>
+          <span style={{color:"red",textDecoration:"line-through"}}>
+            {key.work}
+          </span>
+          </>
+        ):(
+         <>
+         {key.work}
+         </>
+        )
+        }
+          </td>
         <td>
           <button onClick={()=>{dispatch(RemoveTask({id:key.id}))}}>remove</button>
         </td>
         <td>
           <button onClick={()=>{dispatch(ReByIndex({id:index}))}}>delete</button>
+        </td>
+        <td>
+          <button onClick={()=>{dispatch(taskComplete({id:key.id}))}}>complete</button>
+        </td>
+        <td>
+          <button onClick={()=>{dispatch(taskInComplete({id:key.id}))}}>incomplete</button>
         </td>
       </tr>
       </>
@@ -29,13 +47,15 @@ const App=()=>{
     <>
     <h1>Todo App!!</h1>
     Enter Task: <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}} /><br/><br/>
-    <button onClick={()=>{dispatch(addTask({id:Date.now(),work:val}))}}>Add</button><br/>
+    <button onClick={()=>{dispatch(addTask({id:Date.now(),work:val,taskStatus:false}))}}>Add</button><br/>
     <table border="1" width="500px">
       <tr>
         <th>Sno</th>
         <th>Task</th>
         <th>Remove</th>
         <th>Delete</th>
+        <th>Complete</th>
+        <th>Incomplete</th>
       </tr>
       {ans}
     </table>
