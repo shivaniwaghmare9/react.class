d7bdd247a31a7549a2a30cf97602bedabd060e9d
shivani <shivaniw69@gmail.com>
2025-06-17 00:52:14 +0530

    App

--- homeapp/src/App.jsx
+++ homeapp/src/App.jsx
@@ -1,10 +1,36 @@
 
 import { useSelector,useDispatch } from "react-redux";
 import { addTask } from "./todoSlice";
+import { useState } from "react";
 const App=()=>{ 
+  const data=useSelector(state=>state.todo.task)
+  const dispatch=useDispatch();
+  const[val,setVal]=useState("")
+  console.log(data);
+  let sno=0;
+  const ans=data.map((key)=>{
+    sno++;
+    return(
+      <>
+      <tr>
+        <td>{sno}</td>
+        <td>{key.task}</td>
+      </tr>
+      </>
+    )
+  })
   return(
     <>
     <h1>Todo App!!</h1>
+    Enter Task: <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}} /><br/><br/>
+    <button onClick={()=>{dispatch(addTask({task:val}))}}>Add</button><br/>
+    <table>
+      <tr>
+        <th>Sno</th>
+        <th>Task</th>
+      </tr>
+      {ans}
+    </table>
     </>
   )
 }
