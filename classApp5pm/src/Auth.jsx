e6193ff8853e6b19edf2d15cd4e38ccc6ee98c8e
shivani <shivaniw69@gmail.com>
2025-06-16 16:58:47 +0530

    authpage

--- classApp5pm/src/Auth.jsx
+++ classApp5pm/src/Auth.jsx
@@ -1,11 +1,7 @@
-import { useContext } from "react";
-import { MyContext } from "./Logincontext";
+
 const Auth=()=>{
-    const{user,logout}=useContext(MyContext)
     return(
         <>
-           <h1>Welcome to :{user.name}</h1>
-           <button onClick={logout}>Logout</button>
         </>
     )
 }
--- classApp5pm/src/UnAuth.jsx
+++ classApp5pm/src/UnAuth.jsx
@@ -1,15 +1,14 @@
 
 import { useContext,useState } from "react";
 import { MyContext } from "./Logincontext";
-const UnAuth=()=>{
+const Unauth=()=>{
   const[name,setName]=useState("")
-   const{login}=useContext(MyContext);
-    return(
-        <>
-          
-          Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/><br/><br/>
-          <button onClick={()=>{login(name)}}>Login</button>
-        </>
-    )
+  const{login}=useContext(MyContext);
+  return(
+    <>
+     Enter Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/><br/><br/>
+     <button onClick={()=>{login(name)}}>Login</button>
+    </>
+  )
 }
-export default UnAuth;
+export default Unauth;
