e9f6a7deb93c0c8d2179f94865d7aacf7036d557
shivani <shivaniw69@gmail.com>
2025-06-17 01:33:31 +0530

    todocomplete

--- homeapp/src/App.jsx
+++ homeapp/src/App.jsx
@@ -1,11 +1,22 @@
 
 import { useSelector,useDispatch } from "react-redux";
-import { addTask,RemoveTask,ReByIndex,taskComplete,taskInComplete } from "./todoSlice";
+import { addTask,RemoveTask,ReByIndex,taskComplete,taskInComplete,myEdit } from "./todoSlice";
 import { useState } from "react";
 const App=()=>{ 
   const data=useSelector(state=>state.todo.task)
   const dispatch=useDispatch();
   const[val,setVal]=useState("")
+  const[btn,setBtn]=useState(true);
+  const[myid,setMyid]=useState("");
+  const dataEdit=(key,work)=>{
+    setVal(work)
+    setBtn(false)
+    setMyid(id)
+  }
+  const editData=()=>{
+    dispatch(myEdit({id:myid,work:val}))
+    setBtn(true)
+  }
   console.log(data);
   let sno=0;
   const ans=data.map((key,index)=>{
@@ -39,6 +50,9 @@ const App=()=>{
         <td>
           <button onClick={()=>{dispatch(taskInComplete({id:key.id}))}}>incomplete</button>
         </td>
+        <td>
+          <button onClick={()=>{dataEdit(key.id,key.work)}}>Edit</button>
+        </td>
       </tr>
       </>
     )
@@ -47,7 +61,16 @@ const App=()=>{
     <>
     <h1>Todo App!!</h1>
     Enter Task: <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}} /><br/><br/>
-    <button onClick={()=>{dispatch(addTask({id:Date.now(),work:val,taskStatus:false}))}}>Add</button><br/>
+    {btn?(
+      <>
+      <button onClick={()=>{dispatch(addTask({id:Date.now(),work:val,taskStatus:false}))}}>Add</button><br/>
+      </>
+    ):(
+      <>
+       <button onClick={editData}>EditSave</button>
+      </>
+    )}
+    
     <table border="1" width="500px">
       <tr>
         <th>Sno</th>
@@ -56,6 +79,7 @@ const App=()=>{
         <th>Delete</th>
         <th>Complete</th>
         <th>Incomplete</th>
+        <th>Edit</th>
       </tr>
       {ans}
     </table>
