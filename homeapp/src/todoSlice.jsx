f00bad817ff29afd9cd3389964c6772962eafc9b
shivani <shivaniw69@gmail.com>
2025-06-17 00:46:36 +0530

    slice

---
+++ homeapp/src/todoSlice.jsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoSlice=createSlice({
+    name:"todo",
+    initialState:{
+        task:[]
+    },
+    reducers:{
+        addTask:(state,actions)=>{
+            console.log(actions)
+            state.task.push(actions.payload)
+        }
+    }
+})
+export const{addTask}=todoSlice.actions;
+export default todoSlice.reducer;
