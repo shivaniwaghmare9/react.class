2bc6a134870f42d8e76861a4521d9374a63f7268
shivani <shivaniw69@gmail.com>
2025-06-18 22:55:00 +0530

    App

--- homeapp/src/App.jsx
+++ homeapp/src/App.jsx
@@ -1,21 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 
-import { useSelector,useDispatch } from "react-redux";
-import { flipCount } from "./coinSlice";
 const App=()=>{
-  const {coin,count,headCount,tailCount}=useSelector(state=>state.mycoin)
-  const dispatch=useDispatch();
   return(
     <>
-      <h1>Flip Coin</h1>
-      <div>
-        {coin}
-      </div>
-      <div>
-        <h3>FlipCount:{count}</h3>
-        <h3>HeadsCount:{headCount}</h3>
-        <h3>TailsCount:{tailCount}</h3>
-      </div>
-      <button onClick={()=>{dispatch(flipCount())}}>Click</button>
+     <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Layout/>}>
+            
+
+            </Route>
+         </Routes>
+     </BrowserRouter>
     </>
   )
 }
--- homeapp/src/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import coinReducer from "./coinSlice"
-const store=configureStore({
-  reducer:{
-     mycoin:coinReducer
-  }
-})
-export default store;
