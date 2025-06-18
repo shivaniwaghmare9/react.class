6373d724c603ed72e78b3ef813007c1df2df8fd1
shivani <shivaniw69@gmail.com>
2025-06-19 00:12:01 +0530

    search

--- homeapp/src/pages/Search.jsx
+++ homeapp/src/pages/Search.jsx
@@ -1,4 +1,5 @@
 
+import Table from 'react-bootstrap/Table';
 import axios from "axios";
 import { useState } from "react";
 const Search=()=>{
@@ -9,12 +10,44 @@ const Search=()=>{
         const response=await axios.get(api);
         setMydata(response.data);
     }
+    let sno=0;
+    const ans=mydata.map((key)=>{
+        sno++;
+        return(
+            <>
+            <tr>
+                <td>{sno}</td>
+                <td>{key.name}</td>
+                <td>{key.productno}</td>
+                <td>{key.productname}</td>
+                <td>{key.qty}</td>
+                <td>{key.price}</td>
+            </tr>
+            </>
+        )
+    })
 
     return(
         <>
          <h1>Search product</h1>
          Enter Product Name: <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/><br/><br/>
-         <button onClick={handleSearch}>Search</button>
+         <button onClick={handleSearch}>Search</button><br/><br/>
+         <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Sno</th>
+          <th>Customber Name</th>
+          <th>Product NO</th>
+          <th>Product Name</th>
+          <th>Product Qty</th>
+          <th>Net Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ans}
+      </tbody>
+      </Table>
+
         </>
     )
 }
