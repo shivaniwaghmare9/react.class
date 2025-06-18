c07d2d92b2fc6eae0e8aaac847ff738dae80a5a2
shivani <shivaniw69@gmail.com>
2025-06-18 23:09:01 +0530

    json

--- homeapp/src/pages/Insert.jsx
+++ homeapp/src/pages/Insert.jsx
@@ -1,9 +1,44 @@
 
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState } from "react";
 const Insert=()=>{
+    const [input,setInput]=useState({});
+    const handleSubmit=()=>{
+        let api="http://localhost:3000/product"
+    }
     return(
         <>
         <h1>Insert Data!!</h1>
+        <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Customber name</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Product no</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Product name</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Product QTY</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Product Price</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
         </>
     )
 }
