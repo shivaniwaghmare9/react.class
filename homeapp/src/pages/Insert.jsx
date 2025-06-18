571f2b872771570444082403e390d6b17c4e0c3c
shivani <shivaniw69@gmail.com>
2025-06-18 23:43:24 +0530

    form

--- homeapp/src/pages/Insert.jsx
+++ homeapp/src/pages/Insert.jsx
@@ -2,39 +2,49 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from "react";
+import axios from 'axios';
 const Insert=()=>{
     const [input,setInput]=useState({});
-    const handleSubmit=()=>{
-        let api="http://localhost:3000/product"
+    const handleInput=(e)=>{
+        let name=e.target.name;
+        let value=e.target.value;
+        setInput(Values=>({...Values,[name]:value}))
+        console.log(input)
+    }
+    const handleSubmit=async()=>{
+        let api="http://localhost:3000/product";
+        const response=await axios.post(api,input);
+        console.log(response.data);
+        alert("data succesfully inserted!")
     }
     return(
         <>
         <h1>Insert Data!!</h1>
         <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" >
         <Form.Label>Customber name</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="text" name="name" onChange={handleInput}/>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" >
         <Form.Label>Product no</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="text" name="no" onChange={handleInput}/>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" >
         <Form.Label>Product name</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="text" name="product name" onChange={handleInput}/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" >
         <Form.Label>Product QTY</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="text" name="qty"onChange={handleInput} />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" >
         <Form.Label>Product Price</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="text" name="price" onChange={handleInput}/>
       </Form.Group>
       
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onChange={handleSubmit}>
         Submit
       </Button>
     </Form>
