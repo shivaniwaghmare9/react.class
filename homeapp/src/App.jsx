bbedfebcc9f99610c03bba65aa4b5fe9c9addd06
shivani <shivaniw69@gmail.com>
2025-06-20 23:16:44 +0530

    app

--- homeapp/src/App.jsx
+++ homeapp/src/App.jsx
@@ -7,8 +7,8 @@ const App=()=>{
   return(
     <>
     <h3>React redux toolkit!!</h3><br/>
-    <button>Click</button><br/><br/>
-    <div style={{width:"200px",height:"200px",backgroundColor:"red"}}>
+    <button onClick={()=>{dispatch(bglClrChange())}}>Click</button><br/><br/>
+    <div style={{width:"200px",height:"200px",backgroundColor:myclr}}>
 
     </div>
     </>
