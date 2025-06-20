a895c114713050ebdc2eb4ef7b0241a8e813a65d
shivani <shivaniw69@gmail.com>
2025-06-20 23:21:16 +0530

    app

--- homeapp/src/App.jsx
+++ homeapp/src/App.jsx
@@ -1,13 +1,16 @@
 
 import { useSelector,useDispatch } from "react-redux";
 import { bglClrChange } from "./colorSlice";
+import { useState } from "react";
 const App=()=>{
+  cosnt[clr,setClr]=useState("")
   const myclr=useSelector(state=>state.mycolor.color)
   const dispatch=useDispatch();
   return(
     <>
     <h3>React redux toolkit!!</h3><br/>
-    <button onClick={()=>{dispatch(bglClrChange())}}>Click</button><br/><br/>
+    Enter Color: <input type="text" value={clr} onChange={(e)=>{setClr(e.target.value)}}/><br/><br/>
+    <button onClick={()=>{dispatch(bglClrChange(clr))}}>Click</button><br/><br/>
     <div style={{width:"200px",height:"200px",backgroundColor:myclr}}>
 
     </div>
