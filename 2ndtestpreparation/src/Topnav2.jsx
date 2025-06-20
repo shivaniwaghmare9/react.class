54a1104741e951a0b9df17af6f1fe7364aae26ac
shivani <shivaniw69@gmail.com>
2025-06-20 22:47:59 +0530

    top

---
+++ 2ndtestpreparation/src/Topnav2.jsx
@@ -0,0 +1,28 @@
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+const Topnav=()=>{
+    return(
+        <>
+         <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="home">Home</Nav.Link>
+            <Nav.Link as={Link} to="insert">Insert</Nav.Link>
+            <Nav.Link as={Link} to="display">Display</Nav.Link>
+            <Nav.Link as={Link} to="search">Search</Nav.Link>
+            <Nav.Link as={Link} to="update">Update</Nav.Link>
+            <Nav.Link as={Link} to="qnty">Total QTY</Nav.Link>
+            <Nav.Link as={Link} to="contact">Contact Us</Nav.Link>
+            
+          </Nav>
+        </Container>
+      </Navbar>
+        </>
+    )
+}
+export default Topnav;
