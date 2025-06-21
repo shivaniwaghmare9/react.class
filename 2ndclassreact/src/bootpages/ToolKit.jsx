489c5ec87c0cb05aba0ac88fde04983aed04e49e
shivani <shivaniw69@gmail.com>
2025-06-21 23:17:02 +0530

    toll

--- 2ndclassreact/src/bootpages/ToolKit.jsx
+++ 2ndclassreact/src/bootpages/ToolKit.jsx
@@ -1,9 +1,14 @@
 
-
+import { useSelector,useDispatch } from "react-redux";
+import { Increment,Decrement } from "../TodoSlices/counterSlice";
 const ToolKit=()=>{
+    const work=useSelector(state=>state.mycounter.count)
+    const dispatch=useDispatch();
+
     return(
         <>
            <h3>Counter App!!</h3>
+           <h4>Count:{work}</h4>
         </>
     )
 }
