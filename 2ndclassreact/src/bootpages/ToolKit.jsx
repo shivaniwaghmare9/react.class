dc861d135479498ae7adbdcbc7680b7be0d306be
shivani <shivaniw69@gmail.com>
2025-06-22 00:02:50 +0530

    toolkit

--- 2ndclassreact/src/bootpages/ToolKit.jsx
+++ 2ndclassreact/src/bootpages/ToolKit.jsx
@@ -1,32 +1,55 @@
 
-//====================================================COUNTER======================================================================
-// import { useSelector,useDispatch } from "react-redux";
-// import { Increment,Decrement } from "../TodoSlices/counterSlice";
-// const ToolKit=()=>{
-//     const work=useSelector(state=>state.mycounter.count)
-//     const dispatch=useDispatch();
+// //====================================================COUNTER======================================================================
+// // import { useSelector,useDispatch } from "react-redux";
+// // import { Increment,Decrement } from "../TodoSlices/counterSlice";
+// // const ToolKit=()=>{
+// //     const work=useSelector(state=>state.mycounter.count)
+// //     const dispatch=useDispatch();
 
-//     return(
-//         <>
-//            <h3>Counter App!!</h3>
-//            <h4>Count:{work}</h4>
-//            <button onClick={()=>{dispatch(Increment())}}>Increment</button>
-//            <button onClick={()=>{dispatch(Decrement())}}>Decrement</button>
-//         </>
-//     )
-// }
-// export default ToolKit;
-//==================================================COLORSLICE========================================================
+// //     return(
+// //         <>
+// //            <h3>Counter App!!</h3>
+// //            <h4>Count:{work}</h4>
+// //            <button onClick={()=>{dispatch(Increment())}}>Increment</button>
+// //            <button onClick={()=>{dispatch(Decrement())}}>Decrement</button>
+// //         </>
+// //     )
+// // }
+// // export default ToolKit;
+// //==================================================COLORSLICE========================================================
+
+// // import { useSelector,useDispatch } from "react-redux";
+// // import { changeClr } from "../TodoSlices/colorSlice";
+// // const ToolKit=()=>{
+// //     const clr=useSelector(state=>state.mycolor.color)
+// //     const dispatch=useDispatch();
+// //     return(
+// //         <>
+// //          <h3>Change Color!!</h3>
+// //          <button onClick={()=>{dispatch(changeClr())}}>Change</button><br/><br/>
+// //          <div style={{width:"200px",height:"200px",backgroundColor:clr}}>
+
+// //          </div>
+         
+// //         </>
+// //     )
+// // }
+// // export default ToolKit;
+
+// //==============================================THEMECHANGE(COLOR)=========================================================================================
 
 // import { useSelector,useDispatch } from "react-redux";
 // import { changeClr } from "../TodoSlices/colorSlice";
+// import { useState } from "react";
 // const ToolKit=()=>{
+//     const[myclr,setClr]=useState("")
 //     const clr=useSelector(state=>state.mycolor.color)
 //     const dispatch=useDispatch();
 //     return(
 //         <>
 //          <h3>Change Color!!</h3>
-//          <button onClick={()=>{dispatch(changeClr())}}>Change</button><br/><br/>
+//          Enter Color: <input type="text" value={myclr} onChange={(e)=>{setClr(e.target.value)}}/><br/><br/>
+//          <button onClick={()=>{dispatch(changeClr(myclr))}}>Change</button><br/><br/>
 //          <div style={{width:"200px",height:"200px",backgroundColor:clr}}>
 
 //          </div>
@@ -36,24 +59,12 @@
 // }
 // export default ToolKit;
 
-//==============================================THEMECHANGE(COLOR)=========================================================================================
+//=================================================================TODO=====================================================
 
-import { useSelector,useDispatch } from "react-redux";
-import { changeClr } from "../TodoSlices/colorSlice";
-import { useState } from "react";
 const ToolKit=()=>{
-    const[myclr,setClr]=useState("")
-    const clr=useSelector(state=>state.mycolor.color)
-    const dispatch=useDispatch();
     return(
         <>
-         <h3>Change Color!!</h3>
-         Enter Color: <input type="text" value={myclr} onChange={(e)=>{setClr(e.target.value)}}/><br/><br/>
-         <button onClick={()=>{dispatch(changeClr(myclr))}}>Change</button><br/><br/>
-         <div style={{width:"200px",height:"200px",backgroundColor:clr}}>
-
-         </div>
-         
+        <h1>Todo App!!</h1>
         </>
     )
 }
