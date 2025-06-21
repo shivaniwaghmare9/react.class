eecfc1a95166f3bc66ad1487c0f1cb5e8ad4290d
shivani <shivaniw69@gmail.com>
2025-06-21 23:23:44 +0530

    main

--- 2ndclassreact/src/main.jsx
+++ 2ndclassreact/src/main.jsx
@@ -5,11 +5,16 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/bootstyle.css"
 import "./style.css"
-import UserContext from './componenet/UserContext.jsx';
+//import UserContext from './componenet/UserContext.jsx';
+import store from './store.jsx';
+import { Provider } from 'react-redux';
 createRoot(document.getElementById('root')).render(
- <UserContext>
-   <App />
- </UserContext>
+//  <UserContext>
+//    <App />
+//  </UserContext>
+ <Provider store={store}>
+  <App/>
+ </Provider>
     
   
 )
