aaba26f24e5e1a5d2e9c620c6759509eb6cf248c
shivani <shivaniw69@gmail.com>
2025-06-21 23:36:10 +0530

    coloslice

---
+++ 2ndclassreact/src/TodoSlices/colorSlice.jsx
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const colorSlice=createSlice({
+    name:"mycolor",
+    initialState:{
+        color:"blue"
+    },
+    reducers:{
+        changeClr:(state)=>{
+            state.color="red"
+        }
+    }
+})
+export const {changeClr}=colorSlice.actions;
+export default colorSlice.reducer;
