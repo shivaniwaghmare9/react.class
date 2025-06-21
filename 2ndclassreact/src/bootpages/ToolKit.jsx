8d89bc532ddd737e1861fb10bdfbb84e3e505c82
shivani <shivaniw69@gmail.com>
2025-06-21 23:52:53 +0530

    app

--- 2ndclassreact/src/bootpages/ToolKit.jsx
+++ 2ndclassreact/src/bootpages/ToolKit.jsx
@@ -1,16 +1,36 @@
 
-import { useSelector,useDispatch } from "react-redux";
-import { Increment,Decrement } from "../TodoSlices/counterSlice";
-const ToolKit=()=>{
-    const work=useSelector(state=>state.mycounter.count)
-    const dispatch=useDispatch();
+//====================================================COUNTER======================================================================
+// import { useSelector,useDispatch } from "react-redux";
+// import { Increment,Decrement } from "../TodoSlices/counterSlice";
+// const ToolKit=()=>{
+//     const work=useSelector(state=>state.mycounter.count)
+//     const dispatch=useDispatch();
 
+//     return(
+//         <>
+//            <h3>Counter App!!</h3>
+//            <h4>Count:{work}</h4>
+//            <button onClick={()=>{dispatch(Increment())}}>Increment</button>
+//            <button onClick={()=>{dispatch(Decrement())}}>Decrement</button>
+//         </>
+//     )
+// }
+// export default ToolKit;
+//==================================================COLORSLICE========================================================
+
+import { useSelector,useDispatch } from "react-redux";
+import { changeClr } from "../TodoSlices/colorSlice";
+const ToolKit=()=>{
+    const clr=useSelector(state=>state.mycolor.color)
+    const dispatch=useDispatch();
     return(
         <>
-           <h3>Counter App!!</h3>
-           <h4>Count:{work}</h4>
-           <button onClick={()=>{dispatch(Increment())}}>Increment</button>
-           <button onClick={()=>{dispatch(Decrement())}}>Decrement</button>
+         <h3>Change Color!!</h3>
+         <button onClick={()=>{dispatch(changeClr())}}>Change</button><br/><br/>
+         <div style={{width:"200px",height:"200px",backgroundColor:clr}}>
+
+         </div>
+         
         </>
     )
 }
