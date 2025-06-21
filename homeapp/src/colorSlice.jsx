306b5b5751c45e1354bf2ba94e514669bcde89bf
shivani <shivaniw69@gmail.com>
2025-06-21 17:55:26 +0530

    slice

--- homeapp/src/colorSlice.jsx
+++ homeapp/src/colorSlice.jsx
@@ -7,6 +7,7 @@ const colorSlice=createSlice({
     },
     reducers:{
         bgClrChange:(state,actions)=>{
+            console.log(actions.payload)
             state.color=(actions.payload)
         }
     }
