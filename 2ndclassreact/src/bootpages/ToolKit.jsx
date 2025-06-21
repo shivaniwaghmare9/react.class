2d229df9eec18c8c674623a6ce7f7f962152b76d
shivani <shivaniw69@gmail.com>
2025-06-21 23:56:55 +0530

    tool

--- 2ndclassreact/src/bootpages/ToolKit.jsx
+++ 2ndclassreact/src/bootpages/ToolKit.jsx
@@ -18,14 +18,37 @@
 // export default ToolKit;
 //==================================================COLORSLICE========================================================
 
+// import { useSelector,useDispatch } from "react-redux";
+// import { changeClr } from "../TodoSlices/colorSlice";
+// const ToolKit=()=>{
+//     const clr=useSelector(state=>state.mycolor.color)
+//     const dispatch=useDispatch();
+//     return(
+//         <>
+//          <h3>Change Color!!</h3>
+//          <button onClick={()=>{dispatch(changeClr())}}>Change</button><br/><br/>
+//          <div style={{width:"200px",height:"200px",backgroundColor:clr}}>
+
+//          </div>
+         
+//         </>
+//     )
+// }
+// export default ToolKit;
+
+//==============================================THEMECHANGE(COLOR)=========================================================================================
+
 import { useSelector,useDispatch } from "react-redux";
 import { changeClr } from "../TodoSlices/colorSlice";
+import { useState } from "react";
 const ToolKit=()=>{
+    const[myclr,setClr]=useState("")
     const clr=useSelector(state=>state.mycolor.color)
     const dispatch=useDispatch();
     return(
         <>
          <h3>Change Color!!</h3>
+         Enter Color: <input type="text" value={myclr} onChange={(e)=>{setClr(e.target.value)}}/><br/><br/>
          <button onClick={()=>{dispatch(changeClr())}}>Change</button><br/><br/>
          <div style={{width:"200px",height:"200px",backgroundColor:clr}}>
 
