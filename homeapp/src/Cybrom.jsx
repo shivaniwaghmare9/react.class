e3daee09d1840a158c2236aedb624b6ffd7e5b2f
shivani <shivaniw69@gmail.com>
2025-06-23 11:54:45 +0530

    cybrom

--- homeapp/src/Cybrom.jsx
+++ homeapp/src/Cybrom.jsx
@@ -1,2 +1,12 @@
 
-const 
+import { useContext } from "react";
+import { MyContext } from "./UserContext";
+const Cybrom=()=>{
+    const{user,setUser}=useContext(MyContext)
+    return(
+        <>
+          <h1>Welcome to:{user}</h1>
+        </>
+    )
+}
+export default Cybrom;
--- homeapp/src/main.jsx
+++ homeapp/src/main.jsx
@@ -2,11 +2,13 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
-
+import UserContext from './UserContext.jsx'
 createRoot(document.getElementById('root')).render(
    
-  
-       <App />
+  <UserContext>
+     <App />
+  </UserContext>
+       
   
 
        
